36f0201341161053283c81904c7826cd8b2807a3
NijjohUnno <nigelamaya@gmail.com>
2023-03-06 22:28:52 +0300

    Added name input

--- BMIcalc.py
+++ BMIcalc.py
@@ -1,17 +1,18 @@
+name = input("Enter your name ")
 weight = int(input("Input weight in pounds: "))
 height = float(input("Input height in inches: "))
 
 bmi = (weight * 703) / (height * height)
 
 if bmi <= 18.5:
-    print(f'Your BMI is {bmi}. You are underweight with minimal health risk')
+    print(f'Hi {name} your BMI is {bmi}. You are underweight with minimal health risk')
 elif bmi >= 18.5:
-    print(f'Your BMI is {bmi}. You are normal weight with minimal health risk')
+    print(f'Hi {name} your BMI is {bmi}. You are normal weight with minimal health risk')
 elif bmi >= 25:
-    print(f'Your BMI is {bmi}. You are overweight with increased health risk')
+    print(f'Hi {name} your BMI is {bmi}. You are overweight with increased health risk')
 elif bmi >= 30:
-    print(f'Your BMI is {bmi}. You are obese with high health risk')
+    print(f'Hi {name} your BMI is {bmi}. You are obese with high health risk')
 elif bmi >=35:
-    print(f'Your BMI is {bmi}. You are severely obese with very high health risk')
+    print(f'Hi {name} your BMI is {bmi}. You are severely obese with very high health risk')
 else:
-    print(f'Your BMI is {bmi}. You are Morbidly obese with extremely high health risk')
+    print(f'Hi {name} your BMI is {bmi}. You are Morbidly obese with extremely high health risk')
