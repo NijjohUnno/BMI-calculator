69e28a47d5e553d800f0ea426e54ba25018c8fd1
NijjohUnno <nigelamaya@gmail.com>
2023-03-08 21:54:23 +0300

    comment

--- BMIcalc.py
+++ BMIcalc.py
@@ -6,8 +6,8 @@ bmi = (weight * 703) / (height * height)
 
 if bmi <= 18.5:
     print(f'Hi {name} your BMI is {bmi}. You are underweight with minimal health risk')
-elif bmi >= 18.5:
-    print(f'Hi {name} your BMI is {bmi}. You are normal weight with minimal health risk')
+#elif bmi >= 18.5:
+    #print(f'Hi {name} your BMI is {bmi}. You are normal weight with minimal health risk')
 elif bmi >= 25:
     print(f'Hi {name} your BMI is {bmi}. You are overweight with increased health risk')
 elif bmi >= 30:
---
+++ trials.py
@@ -0,0 +1,17 @@
+num = [2,4,5,6,7,8]
+sum = 0
+for number in num:    
+    sum += number
+print(sum)
+
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Change for loop to use enumerate() and update print()
+for index, a in enumerate(areas) :
+    print('room ' + str(index) + ": " + str(a))
+
+
+    for x in house :
+    print("the " + x[0] + " is " + str(x[1]) + " sqm")
